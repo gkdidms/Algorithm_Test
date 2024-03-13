c1c3f6ef622c0707dc72187b5061ebe3675f691d
yeeun_ha <67332520+gkdidms@users.noreply.github.com>
2024-03-13 11:12:46 +0000

    2024/03/13 프로그래머스 - 저주의숫자3

---
+++ C++/저주의숫자3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+/*
+int solution(int n);
+
+int main() {
+  int answer = solution(40);
+
+  cout << answer;
+}
+
+int solution(int n) {
+  int answer = 0;
+
+  for (int i =1; i < n + 1; ++i)
+    {
+      ++answer;
+      while (true)
+      {
+          if (answer % 3 != 0 && to_string(answer).find("3") == string::npos)
+              break;
+         ++answer;
+      }
+    }
+  return answer;
+}*/
