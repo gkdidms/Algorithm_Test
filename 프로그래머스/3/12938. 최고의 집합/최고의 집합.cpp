ec4973489d8198876523ff76dde211c832010853
yeeun_ha <67332520+gkdidms@users.noreply.github.com>
2025-04-28 19:26:44 +0900

    [level 3] Title: 최고의 집합, Time: 2.08 ms, Memory: 5.62 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.cpp
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.cpp
@@ -9,13 +9,13 @@ vector<int> solution(int n, int s) {
     if (n > s) answer.push_back(-1);
     else
     {
-        int q = s/n;
-        int r = s % n;
+        int q = s / n;
+        for (int i = 0; i < n; ++i)
+            answer.push_back(q);
         
-        answer.resize(n, q);
-        
-        for (int i = 0; i < r; ++i)
-            answer[i]++;
+        int m = s % n;
+        for (int i = 0; i < m; ++i)
+            answer[i] += 1;
     }
     
     sort(answer.begin(), answer.end());
