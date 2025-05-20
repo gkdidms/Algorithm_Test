1e5e3e2497f8b7491924945e84efe6d8a6f8e3a7
yeeun_ha <67332520+gkdidms@users.noreply.github.com>
2025-05-20 14:39:52 +0900

    [Gold II] Title: 철로, Time: 36 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Gold/13334. 철로/철로.cc
+++ 백준/Gold/13334. 철로/철로.cc
@@ -1,69 +1,60 @@
-#include <cstring>
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 #include <queue>
-
-#define INT 1000000
+#include <iostream>
 
 #define format std::ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr)
 
 using namespace std;
 
-bool cmp(pair<int, int> a, pair<int, int> b) {
-	if (a.second == b.second)
-		return a.first < b.first;
-	else
-		return a.second < b.second;
+int main() {
+  format;
+
+  int n, d;
+  cin >> n;
+
+  vector<pair<int, int>> v(n);
+  for (int i = 0; i < n; ++i)
+    {
+      int h, o;
+      cin >> h >> o;
+      if (h > o)
+        v[i] = {o, h};
+      else v[i] = {h, o};
+    }
+
+  sort(v.begin(), v.end(), [](const auto& a, const auto& b) {
+    if (a.second == b.second) return a.first < b.first;
+    return a.second < b.second;
+  });
+
+  cin >> d;
+  
+  priority_queue<int, vector<int>, greater<int>> pq;
+  int answer = 0;
+  for (int i = 0; i < n; ++i)
+    {
+      int h = v[i].first; // 시작 시간
+      int o = v[i].second; // 끝 시간
+
+      if (o - h <= d) pq.push(h); // 끝 - 시작 <= 길이
+      else continue;
+
+      while(!pq.empty())
+        {
+          if (pq.top() < o - d) pq.pop(); // 시작 < 끝 - 길이(현재 idx의 끝 시간을 기준으로 계산한 범위 안에 포함이 되는가?)
+          else 
+          {
+            answer = max((int)pq.size(), answer);
+            break;
+          }
+        }
+    }
+
+
+  cout << answer;
+
+  return 0;
 }
 
-int main()
-{
-	format;
-
-	int n, l;
-	cin >> n;
-
-	vector<pair<int, int>> v;
-
-	for (int i = 0; i < n; ++i)
-	{
-		int h, o;
-		cin >> h >> o;
-
-		if (h > o)
-			v.push_back({ o, h });
-		else
-			v.push_back({ h, o });
-	}
-
-	cin >> l; //포함되어야 할 거리
-
-	sort(v.begin(), v.end(), cmp);
-
-	priority_queue<int> q;
-
-	int answer = 0;
-
-	for (int i = 0; i < v.size(); ++i)
-	{
-		int h = v[i].first;
-		int o = v[i].second;
-
-		if (o - h <= l) q.push(-h);
-		else continue;
-
-		while (!q.empty())
-		{
-			if (-q.top() < o - l) q.pop();
-			else
-			{
-				answer = max(answer, (int)q.size());
-				break;
-			}
-		}
-	}
-
-	cout << answer << endl;
-}
