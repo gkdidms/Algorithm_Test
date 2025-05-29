165e56214f21a84da9be2ce6090901b4dfe1a7c8
yeeun_ha <67332520+gkdidms@users.noreply.github.com>
2025-05-29 16:22:30 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 8400 ms, Memory: 100456 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -1,47 +1,47 @@
-#include <iostream>
-#include <stdio.h>
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+
+#define format std::ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr)
+#define ll long long
 
 using namespace std;
 
 int main() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
+  format;
 
-	int n;
-	cin >> n;
+  int n;
+  cin >> n;
 
-	int A[4001], B[4001], C[4001], D[4001];
+  int A[4001], B[4001], C[4001], D[4001];
+  for (int i = 0; i < n; ++i)
+    {
+      cin >> A[i] >> B[i] >> C[i] >> D[i];
+    }
 
-	for (int i = 0; i < n; ++i)
-		cin >> A[i] >> B[i] >> C[i] >> D[i];
+  vector<int> sum;
+  for (int i = 0; i < n; ++i)
+    {
+      for (int j = 0; j < n; ++j)
+        sum.push_back(A[i] + B[j]);
+    }
 
-	vector<int> sums;
-	
-	for (int i = 0; i < n; ++i)
-	{
-		int value = 0;
-		for (int j = 0; j < n; ++j)
-		{
-			value = A[i] + B[j];
-			sums.push_back(value);
-		}
-	}
+  sort(sum.begin(), sum.end());
 
-	sort(sums.begin(), sums.end());
-	long long answer = 0;
-	for (int i = 0; i < n; ++i)
-	{
-		int value = 0;
-		for (int j = 0; j < n; ++j)
-		{
-			value = C[i] + D[j];
-			answer += upper_bound(sums.begin(), sums.end(), -value) - lower_bound(sums.begin(), sums.end(), -value);
-		}	
-	}
+  ll answer = 0;
+  for (int i = 0; i < n; ++i)
+    {
+      int value = 0;
+      for (int j = 0; j < n; ++j)
+        {
+          value = C[i] + D[j];
+          answer += upper_bound(sum.begin(), sum.end(), -value) - lower_bound(sum.begin(), sum.end(), -value);
+        }
+    }
 
-	cout << answer;
+  cout << answer;
+
+  return 0;
 }
+
