4972ef5889b2bd3b7e685cb483b0c2b3e8361216
yeeun_ha <67332520+gkdidms@users.noreply.github.com>
2025-09-08 00:59:41 +0900

    [Silver III] Title: 악수, Time: 80 ms, Memory: 80148 KB -BaekjoonHub

---
+++ 백준/Silver/8394. 악수/악수.cc
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+#define format std::ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr)
+
+using namespace std;
+
+int main() {
+  format;
+
+  int n;
+  cin >> n;
+
+  vector<long long> dp(n + 1);
+  dp[1] = 1;
+  dp[2] = 2;
+  for (int i = 3; i <= n; ++i)
+    dp[i] = (dp[i - 2] + dp[i - 1]) % 10;
+
+  
+  
+  cout << dp[n];
+}
