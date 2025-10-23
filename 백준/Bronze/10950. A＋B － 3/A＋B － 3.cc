f71b92ac150a01293e03269cc21652c47e443325
yeeun_ha <67332520+gkdidms@users.noreply.github.com>
2025-10-24 00:11:56 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+#define format std::ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr)
+
+using namespace std;
+
+int main() {
+  format;
+
+  int t;
+  cin >> t;
+
+  while(t--){
+    int a, b;
+    cin >> a>> b;
+
+    cout << a + b << '\n';
+  }
+
+  return 0;
+}
+
