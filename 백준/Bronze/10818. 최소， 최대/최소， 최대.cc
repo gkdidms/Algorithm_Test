e5dd3eb53b9c73818cf82417df4771067a21c80b
yeeun_ha <67332520+gkdidms@users.noreply.github.com>
2025-11-05 00:44:17 +0900

    [Bronze III] Title: 최소, 최대, Time: 168 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+#define format std::ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr)
+
+using namespace std;
+
+int main() {
+  format;
+
+  int n;
+  cin >> n;
+
+  vector<int> arr(n);
+  for (int i = 0; i < n; ++i)
+    cin >> arr[i];
+
+  sort(arr.begin(), arr.end());
+
+  cout << arr[0] << " " << (*arr.rbegin());
+
+  return 0;
+}
+
