28330af610643c6b50fac47cfcc83916814899c3
yeeun_ha <67332520+gkdidms@users.noreply.github.com>
2025-12-19 00:34:47 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 24 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -26,21 +26,20 @@ int main() {
 
   vector<int> answer;
   int index = 0;
-  while(check(arr) == false){
-    int count = 0;
-    // 제거되지 않은 사람만 세면서 k번째 찾기
-    while(count < k){
-      if(arr[index].second == false){  // 아직 제거 안된 사람만 카운트
-        count++;
+  for(int i = 0; i < n; ++i){
+    int cnt = 0;
+    while(cnt < k){
+      if(arr[index].second == false){
+        cnt++;
       }
-      if(count < k){  // k번째 도달 전까지만 이동
+      if(cnt < k){
         index = (index + 1) % n;
       }
     }
+
     arr[index].second = true;
     answer.push_back(arr[index].first);
-    // 다음 시작 위치로 이동
-    index = (index + 1) % n;
+    index = (index + 1) %n ;
   }
 
   cout << "<";
